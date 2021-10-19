fc9d0d88f2893b7ffc4fcd6e0dd92e7fccb21770
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2021-10-19 15:47:12 +0100

    add click actions

---
+++ app.js
@@ -0,0 +1,9 @@
+const navslide = () =>{
+  const burger= document.querySelector('.burger');
+  const nav = document.querySelector('nav-links');
+    burger.addEventListener('click', () => {
+    nav.classList.toggle('.nav-active');
+  });
+}
+
+navslide();
